6c7efea792663c2f7d7ec74249a0a85c12632a87
Muzahid <muzahid@pop-os.localdomain>
2022-10-03 12:47:56 +0600

    database connected using mongoose

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const env = require("dotenv").config();
+const mongoose = require('mongoose');
 const productRoute = require('./routes/tours/v1/tours.router');
 const port = process.env.PORT || 5000;
 
@@ -8,6 +10,18 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// database connection using mongoose
+mongoose.connect(process.env.DATABASE_LOCAL).then(() => {
+    console.log(`database connected using mongoose`);
+});
+
+// schema design
+const toursSchema = mongoose.Schema(
+    {
+
+    }
+);
 
 
 app.use("/api/v1/tours", productRoute);
